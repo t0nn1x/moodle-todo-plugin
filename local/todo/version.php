5683bca114e4c96909bf9b10e80aeba3937b5e5a
t0nn1x <antoonhrobust@gmail.com>
2025-07-25 20:54:37 +0200

    feat: add plugin version configuration

---
+++ local/todo/version.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Version details for the todo local plugin.
+ *
+ * @package    local_todo
+ * @copyright  2025 Anton Khrobust
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->version   = 2025072500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2021051700;        // Requires this Moodle version (3.11).
+$plugin->component = 'local_todo';      // Full name of the plugin (used for diagnostics).
+$plugin->maturity  = MATURITY_ALPHA;    // This version's maturity level.
