5c9ef7bef69b90a459574d5029781320e0abb0f8
t0nn1x <antoonhrobust@gmail.com>
2025-07-26 17:49:17 +0200

    feat: enhance todo index with form handling for creating and editing todos

--- local/todo/index.php
+++ local/todo/index.php
@@ -11,10 +11,12 @@
 require_once(__DIR__ . '/../../config.php');
 require_once(__DIR__ . '/lib.php');
 require_once($CFG->libdir . '/tablelib.php');
+require_once($CFG->libdir . '/formslib.php');
+require_once($CFG->dirroot . '/local/todo/classes/form/todo_form.php');
 
 require_login();
 
-$context =   $context = context_system::instance();
+$context = context_system::instance();
 require_capability('local/todo:view', $context);
 
 $PAGE->set_context($context);
@@ -25,11 +27,43 @@ $PAGE->set_heading(get_string('todolist', 'local_todo'));
 
 echo $OUTPUT->header();
 
-$addurl = new moodle_url('/local/todo/edit.php');
-echo html_writer::div(
-    $OUTPUT->single_button($addurl, get_string('addtodo', 'local_todo'), 'get', ['class' => 'btn btn-primary']),
-    'mb-3'
-);
+$id = optional_param('id', 0, PARAM_INT);
+$editing = false;
+$todo = null;
+if ($id) {
+    $todo = local_todo_get_todo($id);
+    if ($todo && local_todo_can_manage_todo($id)) {
+        $editing = true;
+    } else {
+        $todo = null;
+    }
+}
+
+$mform = new \local_todo\form\todo_form();
+if ($editing && $todo) {
+    $mform->set_data($todo);
+}
+
+if ($mform->is_cancelled()) {
+    redirect(new moodle_url('/local/todo/index.php'));
+} else if ($data = $mform->get_data()) {
+    if (!empty($data->id)) {
+        // update existing todo
+        if (local_todo_update_todo($data->id, $data)) {
+            redirect(new moodle_url('/local/todo/index.php'), get_string('todoupdated', 'local_todo'));
+        } else {
+            echo $OUTPUT->notification('Failed to update todo', 'error');
+        }
+    } else {
+        // create new todo
+        $newid = local_todo_create_todo($data);
+        if ($newid) {
+            redirect(new moodle_url('/local/todo/index.php'), get_string('todoadded', 'local_todo'));
+        } else {
+            echo $OUTPUT->notification('Failed to create todo', 'error');
+        }
+    }
+}
 
 // get user's todos
 $todos = local_todo_get_user_todos($USER->id);
@@ -58,7 +92,7 @@ if (empty($todos)) {
         $actions = [];
 
         if (has_capability('local/todo:manage', $context)) {
-            $editurl = new moodle_url('/local/todo/edit.php', ['id' => $todo->id]);
+            $editurl = new moodle_url('/local/todo/index.php', ['id' => $todo->id]);
             $actions[] = html_writer::link(
                 $editurl,
                 get_string('edit', 'local_todo'),
@@ -80,5 +114,12 @@ if (empty($todos)) {
 
     echo html_writer::table($table);
 }
+
+// display the form below the table
+$formtitle = $editing ? get_string('edittodo', 'local_todo') : get_string('addtodo', 'local_todo');
+echo html_writer::start_div('todo-form-wrapper', ['style' => 'border: 1px solid #ccc; border-radius: 8px; padding: 24px; margin-top: 24px; background: #fafafa;']);
+echo html_writer::tag('h3', $formtitle, ['class' => 'mt-4']);
+$mform->display();
+echo html_writer::end_div();
 
 echo $OUTPUT->footer();
